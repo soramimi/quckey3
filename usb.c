c58ae2a12746393d481c5a98e9afd270f9d7471b
soramimi <fi7s-fct@asahi-net.or.jp>
2020-11-27 00:34:11 +0900

    fix report descriptor

--- usb.c
+++ usb.c
@@ -114,38 +114,38 @@ PROGMEM static const uint8_t keyboard_hid_report_desc[] = {
 	0x09, 0x06,          // Usage (Keyboard),
 	0xa1, 0x01,          // Collection (Application),
 
-	0x75, 0x01,          //   Report Size (1),
-	0x95, 0x08,          //   Report Count (8),
 	0x05, 0x07,          //   Usage Page (Key Codes),
 	0x19, 0xe0,          //   Usage Minimum (224),
 	0x29, 0xe7,          //   Usage Maximum (231),
 	0x15, 0x00,          //   Logical Minimum (0),
 	0x25, 0x01,          //   Logical Maximum (1),
+	0x95, 0x08,          //   Report Count (8),
+	0x75, 0x01,          //   Report Size (1),
 	0x81, 0x02,          //   Input (Data, Variable, Absolute), ;Modifier byte
 
 	0x95, 0x01,          //   Report Count (1),
 	0x75, 0x08,          //   Report Size (8),
-	0x81, 0x03,          //   Input (Constant),                 ;Reserved byte
+	0x81, 0x01,          //   Input (Constant),                 ;Reserved byte
 
-	0x95, 0x06,          //   Report Count (6),
-	0x75, 0x08,          //   Report Size (8),
-	0x15, 0x00,          //   Logical Minimum (0),
-	0x25, 0xdf,          //   Logical Maximum(223),
 	0x05, 0x07,          //   Usage Page (Key Codes),
 	0x19, 0x00,          //   Usage Minimum (0),
-	0x29, 0xdf,          //   Usage Maximum (223),
-	0x81, 0x00,          //   Input (Data, Array),
+	0x29, 0xff,          //   Usage Maximum (255),
+	0x15, 0x00,          //   Logical Minimum (0),
+	0x26, 0xff, 0x00,    //   Logical Maximum(255),
+	0x95, 0x06,          //   Report Count (6),
+	0x75, 0x08,          //   Report Size (8),
+	0x81, 0x00,          //   Input (Data, Array, Absolute),
 
-	0x95, 0x05,          //   Report Count (5),
-	0x75, 0x01,          //   Report Size (1),
 	0x05, 0x08,          //   Usage Page (LEDs),
 	0x19, 0x01,          //   Usage Minimum (1),
 	0x29, 0x05,          //   Usage Maximum (5),
-	0x91, 0x02,          //   Output (Data, Variable, Absolute), ;LED report
+	0x95, 0x05,          //   Report Count (5),
+	0x75, 0x01,          //   Report Size (1),
+	0x91, 0x02,          //   Output (Data, Variable, Absolute, NonVolatile), ;LED report
 
 	0x95, 0x01,          //   Report Count (1),
 	0x75, 0x03,          //   Report Size (3),
-	0x91, 0x03,          //   Output (Constant),                 ;LED report padding
+	0x91, 0x01,          //   Output (Constant),                 ;LED report padding
 
 	0xc0                 // End Collection
 };
@@ -162,13 +162,14 @@ PROGMEM static const uint8_t mouse_hid_report_desc[] = {   /* USB report descrip
 	0x29, 0x05,          //     Usage Maximum (5)
 	0x15, 0x00,          //     Logical Minimum (0)
 	0x25, 0x01,          //     Logical Maximum (1)
-	0x75, 0x01,          //     Report Size (1)
 	0x95, 0x05,          //     Report Count (5)
-	0x81, 0x02,          //     Input (Data,Var,Abs)
+	0x75, 0x01,          //     Report Size (1)
+	0x81, 0x02,          //     Input (Data,Variable,Absolute)
 
-	0x75, 0x03,          //     Report Size (3)
 	0x95, 0x01,          //     Report Count (1)
-	0x81, 0x03,          //     Input (Const,Var,Abs)
+	0x75, 0x03,          //     Report Size (3)
+	0x81, 0x01,          //     Input (Const)
+
 	0x05, 0x01,          //     Usage Page (Generic Desktop)
 	0x09, 0x30,          //     Usage (X)
 	0x09, 0x31,          //     Usage (Y)
@@ -177,7 +178,7 @@ PROGMEM static const uint8_t mouse_hid_report_desc[] = {   /* USB report descrip
 	0x25, 0x7f,          //     Logical Maximum (127)
 	0x75, 0x08,          //     Report Size (8)
 	0x95, 0x03,          //     Report Count (3)
-	0x81, 0x06,          //     Input (Data,Var,Rel)
+	0x81, 0x06,          //     Input (Data,Variable,Relative)
 
 	0xc0,                //   End Collection
 	0xc0,                // End Collection
